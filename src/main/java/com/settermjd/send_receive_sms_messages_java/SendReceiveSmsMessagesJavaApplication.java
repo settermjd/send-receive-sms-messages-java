914c607cac99c67537c6630c2027141dc08ffa55
Matthew Setter <matthew@matthewsetter.com>
2026-05-07 13:19:37 +1000

    Add the ability to send an SMS
    This change adds a route that, when called, will send an SMS to the
    designated phone number. This demo implementation is different to the
    implementation in the other versions of the repository, but it works for
    a Java-based application.

--- src/main/java/com/settermjd/send_receive_sms_messages_java/SendReceiveSmsMessagesJavaApplication.java
+++ src/main/java/com/settermjd/send_receive_sms_messages_java/SendReceiveSmsMessagesJavaApplication.java
@@ -1,22 +1,34 @@
 package com.settermjd.send_receive_sms_messages_java;
 
+import com.twilio.Twilio;
 import com.twilio.twiml.MessagingResponse;
 import com.twilio.twiml.messaging.Body;
 import com.twilio.twiml.messaging.Message;
+import com.twilio.type.PhoneNumber;
 import java.util.Random;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
 
 @SpringBootApplication
 @RestController
 public class SendReceiveSmsMessagesJavaApplication {
+    @Value("${twilio.account_sid}")
+    private String accountSid;
+
+    @Value("${twilio.auth_token}")
+    private String authToken;
+
+    @Value("${twilio.phone_number}")
+    private String phoneNumber;
+
     private static final String defaultOption = "I just wanna tell you how I'm feeling - Gotta make you understand";
     private static final String[] options = {
             "give you up",
@@ -29,6 +41,23 @@ public class SendReceiveSmsMessagesJavaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SendReceiveSmsMessagesJavaApplication.class, args);
+    }
+
+    @PostMapping(
+        path = "/send", 
+        consumes = {MediaType.APPLICATION_FORM_URLENCODED_VALUE}
+    )
+    public @ResponseBody String sendSMS(@RequestParam("recipient") String recipient) {
+        Twilio.init(accountSid, authToken);
+        com.twilio.rest.api.v2010.account.Message message = com.twilio.rest.api.v2010.account.Message
+            .creator(new com.twilio.type.PhoneNumber(recipient),
+                    new com.twilio.type.PhoneNumber(phoneNumber),
+                    "This is the ship that made the Kessel Run in fourteen parsecs?")
+            .create();
+
+        System.out.println("Sent SMS.");
+
+        return message.getBody();
     }
 
     @RequestMapping(method = RequestMethod.POST, path = "/receive/no-response", produces = "application/xml")
